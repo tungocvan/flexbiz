ed8e3824273056ec553bd790d4f9aa6ce63187c2
Tu Ngoc Van <tungocvan@gmail.com>
2026-01-10 11:46:29 +0700

    10012026

--- Modules/Website/resources/views/livewire/components/menu-item.blade.php
+++ Modules/Website/resources/views/livewire/components/menu-item.blade.php
@@ -1,6 +1,21 @@
+@php
+    $currentSlug = request()->route('slug');
+
+    // Active chính nó
+    $isSelfActive = $currentSlug === $category->slug;
+
+    // Active nếu slug nằm trong children
+    $isChildActive = $category->childrenRecursive
+        ->pluck('slug')
+        ->contains($currentSlug);
+
+    // Active tổng
+    $isActive = $isSelfActive || $isChildActive;
+@endphp
+
 @if($category->childrenRecursive->isNotEmpty())
-    <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle"
+    <li class="nav-item dropdown {{ $isActive ? 'active' : '' }}">
+        <a class="nav-link dropdown-toggle {{ $isActive ? 'active' : '' }}"
            href="{{ route('website.products.category', $category->slug) }}"
            id="menu-{{ $category->id }}"
            role="button"
@@ -12,12 +27,14 @@
 
         <div class="dropdown-menu">
             @foreach($category->childrenRecursive as $child)
-                @include('Website::livewire.components.menu-item', ['category' => $child])
+                @include('Website::livewire.components.menu-item', [
+                    'category' => $child
+                ])
             @endforeach
         </div>
     </li>
 @else
-    <a class="dropdown-item nav-link"
+    <a class="dropdown-item {{ $isActive ? 'active' : '' }}"
        href="{{ route('website.products.category', $category->slug) }}">
         {{ $category->name }}
     </a>
--- Modules/Website/resources/views/products/index.blade.php
+++ Modules/Website/resources/views/products/index.blade.php
@@ -3,9 +3,17 @@
 @section('content')
 <h3 class="mb-4">Danh sách sản phẩm</h3>
 <hr>
-@livewire('website.products.product-list', [
-    'categorySlug' => $categorySlug ?? null
-])
+<div class="row">
+    <div class="col-2">
+        @livewire('website.components.nav-menu')
+    </div>
+    <div class="col-10">
+        @livewire('website.products.product-list', [
+                'categorySlug' => $categorySlug ?? null
+            ])
+    </div>
+</div>
+
 
 
 @endsection
