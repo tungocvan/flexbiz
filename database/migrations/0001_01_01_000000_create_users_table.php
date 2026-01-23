9271cacee9d5ed4b1ded5ddae0cd669dcee7dd4a
Tu Ngoc Van <tungocvan@gmail.com>
2026-01-23 12:51:18 +0700

    23012026-1

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -56,22 +56,6 @@ return new class extends Migration
             $table->longText('payload');
             $table->integer('last_activity')->index();
         });
-        Schema::create('user_addresses', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
-
-            $table->string('name'); // Tên người nhận
-            $table->string('phone'); // SĐT người nhận
-
-            // Địa chỉ chi tiết (Tùy project của bạn có tách Xã/Huyện/Tỉnh không, ở đây tôi làm gộp cho gọn, bạn có thể tách nếu cần)
-            $table->string('address')->nullable();
-            $table->string('city')->nullable();
-            $table->string('district')->nullable();
-            $table->string('ward')->nullable();
-
-            $table->boolean('is_default')->default(false);
-            $table->timestamps();
-        });
     }
 
     /**
